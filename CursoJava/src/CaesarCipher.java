4bacdd4d609ebef1cd99effb2b784c239db132a2
Jordi <ynglnk90@gmail.com>
2016-01-27 22:58:51 +0100

    Second part of first exercise

---
+++ CursoJava/src/CaesarCipher.java
@@ -0,0 +1,34 @@
+
+public class CaesarCipher {
+
+	private static final int FIRST_UPPERCASE_LETTER_POSITION = 65;
+	private static final int LAST_UPPERCASE_LETTER_POSITION = 90;
+	private static final int FIRST_LOWERCASE_LETTER_POSITION = 97;
+	private static final int LAST_LOWERCASE_LETTER_POSITION = 122;
+	
+	public String encrypt2(String phrase, int key) {
+		String cipheredMessage = "";
+		
+		int keyParsed = key % 26;
+		
+		for(char charac : phrase.toCharArray()) {
+			if((charac >= FIRST_UPPERCASE_LETTER_POSITION && charac <= LAST_UPPERCASE_LETTER_POSITION)) {
+				cipheredMessage += shiftCharacter(charac, FIRST_UPPERCASE_LETTER_POSITION, keyParsed);
+				continue;
+			}
+			if((charac >= FIRST_LOWERCASE_LETTER_POSITION && charac <= LAST_LOWERCASE_LETTER_POSITION)) {
+				cipheredMessage += shiftCharacter(charac, FIRST_LOWERCASE_LETTER_POSITION, keyParsed);
+				continue;
+			}
+			cipheredMessage += charac;
+		}
+		
+		return cipheredMessage;
+	}
+	
+	private char shiftCharacter(char ch, int firstLetter, int key) {
+		int charResult = ch - firstLetter;
+		int shiftPositions = (charResult + key) % 26;
+		return (char) (firstLetter + shiftPositions);
+	}
+}
---
+++ CursoJava/test/CaesarCipherTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+public class CaesarCipherTest {
+
+	@Test
+	public void testEncrypt() {
+		CaesarCipher cc = new CaesarCipher();
+		String firstStringToCipher = "FIRST LEGION ATTACK EAST FLANK!";
+		String firstStringCiphered = "CFOPQ IBDFLK XQQXZH BXPQ CIXKH!";
+		String firstResult = cc.encrypt2(firstStringToCipher, 23);
+		
+		String secondStringToCipher = "First Legion"; 
+		String secondStringCiphered = "Wzijk Cvxzfe";
+		String secondResult = cc.encrypt2(secondStringToCipher, 17);
+		
+		assertTrue(firstResult.equals(firstStringCiphered));
+		assertTrue(secondResult.equals(secondStringCiphered));
+	}
+}
